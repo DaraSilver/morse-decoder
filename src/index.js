9be78b72cab7954eaf99a94923bf798cabe4d1b6
DaraSilver <dasha98.n@gmail.com>
2023-02-05 23:53:26 +1000

    feat: add test

--- src/index.js
+++ src/index.js
@@ -38,6 +38,55 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
+    let somth = String(expr);
+    let res = []
+    for (let i = 0; i< somth.length; i += 10) {
+        let a = somth.slice(i, i + 10);
+        if (a === '**********') {
+            res.push(12)
+        } else {
+        res.push(Number(a))
+        }
+    }
+    let dots = []
+    for (let i = 0; i< res.length; i ++) {
+        let a = String(res[i])
+        let str = '';
+        for (let j = 0; j< a.length; j += 2) {
+          
+            if (a.slice(j, j + 2) === '10') {
+                str += '.'
+            } else  if (a.slice(j, j + 2) === '11'){
+                
+                str += '-';
+            } 
+                
+            
+        }
+        if (str === '') {
+            dots.push(' ')
+        } else {
+         dots.push(str)
+         }
+
+        
+
+    }
+    let result = ''
+    
+    for (let i = 0; i < dots.length; i++){
+        if (dots[i] !== ' ') {
+            for (let key in MORSE_TABLE) {
+                if (key === dots[i]){
+                    result += MORSE_TABLE[key]
+                }
+            }
+        } else {
+             result += ' '
+            }
+    }
+    return result
+
     // write your solution here
 }
 
